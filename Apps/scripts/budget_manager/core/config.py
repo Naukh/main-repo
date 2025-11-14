b48fe753e774dada47542002089ced72c6abe478
NaumanHaider <62841687+NaumanHaider@users.noreply.github.com>
2025-11-14 23:53:42 +0100

    Fix

--- Apps/scripts/budget_manager/core/config.py
+++ Apps/scripts/budget_manager/core/config.py
@@ -1,7 +1,7 @@
-# core/config.py
 import os
 
-DB_FOLDER = "data"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # core/
+DB_FOLDER = os.path.join(BASE_DIR, "..", "data")
 DB_FILENAME = "budget.db"
 DB_PATH = os.path.join(DB_FOLDER, DB_FILENAME)
 
