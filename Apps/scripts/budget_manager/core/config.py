bac1eec167ee46812e92b4ac1d511f1f19a33327
NaumanHaider <62841687+NaumanHaider@users.noreply.github.com>
2025-11-14 22:18:16 +0100

    Fixes

--- Apps/scripts/budget_manager/core/config.py
+++ Apps/scripts/budget_manager/core/config.py
@@ -1,15 +1,11 @@
 import os
 
 # Determine writable DB path
-if os.getenv("STREAMLIT_APP_TMPDIR"):
-    # Streamlit Cloud environment
-    DB_FOLDER = os.getenv("STREAMLIT_APP_TMPDIR")
-else:
-    # Local environment
-    DB_FOLDER = "db"
 
+DB_FOLDER = "/tmp"  # cloud-writable folder
 DB_FILENAME = "budget.db"
 DB_PATH = os.path.join(DB_FOLDER, DB_FILENAME)
+TABLE_NAME = "entries"
 
 # Table name
 TABLE_NAME = "entries"
